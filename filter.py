f58819e17f080d8e720e24cdf5b92dbc568e94f6
magarcia0 <77859516+magarcia0@users.noreply.github.com>
2021-02-17 10:15:34 -0800

    Add files via upload

---
+++ filter.py
@@ -0,0 +1,11 @@
+#take user input
+user_input = input()
+
+#tokenize each input item on whitespace
+user_input = user_input.split()
+
+#strip non-alphanumeric items
+user_input = [x.strip(',') for x in user_input]
+
+#print to console
+print(*user_input, sep = "\n")
